9b64d3d8405e5f0eca39f5bb03039e62f57ffa36
romuloAMR <amr.rmulo@gmail.com>
2025-09-10 01:38:19 +0000

    feat: init spring

---
+++ src/test/java/com/example/vestigioapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vestigioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
