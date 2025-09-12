cf0bd388100b5725d797c8d2316b9372fa7d805b
romuloAMR <amr.rmulo@gmail.com>
2025-09-12 22:29:09 +0000

    feat: Adding pgadmin to the development environment and database connection settings

--- src/main/java/com/example/vestigioapi/controller/RootController.java
+++ src/main/java/com/example/vestigioapi/controller/RootController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DefaultController {
+public class RootController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "God is my hope";
+        return "God is my hope!";
     }
 }
