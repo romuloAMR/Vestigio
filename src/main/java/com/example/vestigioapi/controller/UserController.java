86d44dfbb60dd3be0e4cd52cb5d3d297893f2119
romuloAMR <amr.rmulo@gmail.com>
2025-09-13 21:23:38 +0000

    fix: fix db connction and first get request

---
+++ src/main/java/com/example/vestigioapi/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.vestigioapi.controller;
+
+import com.example.vestigioapi.model.User;
+import com.example.vestigioapi.repository.UserRepository;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        final List<User> users = repository.findAll();
+        return ResponseEntity.ok(users);
+    }
+    
+}
---
+++ src/main/java/com/example/vestigioapi/util/ValidationMessages.java
@@ -0,0 +1,8 @@
+package com.example.vestigioapi.util;
+
+public class ValidationMessages {
+    public static final String NAME_REQUIRED = "Nome é obrigatório";
+    public static final String EMAIL_REQUIRED = "Email é obrigatório";
+    public static final String EMAIL_INVALID = "Email inválido";
+    public static final String PASSWORD_REQUIRED = "Senha é obrigatório";
+}
