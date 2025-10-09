db5a75dad2bff7293d6864204e9eeee9fe59e99c
Dagson Gabriel <dagson.g.01@gmail.com>
2025-10-09 01:27:11 -0300

    feat: add methods for starting, finishing game sessions and processing questions and answers

--- src/main/java/com/example/vestigioapi/controller/game/session/GameSessionController.java
+++ src/main/java/com/example/vestigioapi/controller/game/session/GameSessionController.java
@@ -2,17 +2,23 @@ package com.example.vestigioapi.controller.game.session;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.vestigioapi.dto.game.move.AnswerRequestDTO;
+import com.example.vestigioapi.dto.game.move.MoveResponseDTO;
+import com.example.vestigioapi.dto.game.move.QuestionRequestDTO;
 import com.example.vestigioapi.dto.game.session.GameSessionCreateDTO;
 import com.example.vestigioapi.dto.game.session.GameSessionResponseDTO;
 import com.example.vestigioapi.model.user.User;
+import com.example.vestigioapi.service.game.move.MoveService;
 import com.example.vestigioapi.service.game.session.GameSessionService;
 
 import jakarta.validation.Valid;
@@ -24,6 +30,8 @@ import lombok.RequiredArgsConstructor;
 public class GameSessionController {
 
     private final GameSessionService gameSessionService;
+    private final MoveService jogadaService;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @PostMapping
     public ResponseEntity<GameSessionResponseDTO> createGame(
@@ -48,4 +56,40 @@ public class GameSessionController {
         GameSessionResponseDTO response = gameSessionService.getGameSessionByRoomCode(roomCode);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/{roomCode}/start")
+    public ResponseEntity<GameSessionResponseDTO> startGameSession(@PathVariable String roomCode, @AuthenticationPrincipal User master) {
+        GameSessionResponseDTO startedGame = gameSessionService.startGameSession(roomCode, master.getId());
+        return ResponseEntity.ok(startedGame);
+    }
+    
+    @PostMapping("/{roomCode}/finish")
+    public ResponseEntity<GameSessionResponseDTO> finishGameSession(@PathVariable String roomCode, @AuthenticationPrincipal User master) {
+        GameSessionResponseDTO finishedGame = gameSessionService.finishGameSession(roomCode, master.getId());
+        return ResponseEntity.ok(finishedGame);
+    }
+
+    @PostMapping("/{roomCode}/ask-question")
+    public ResponseEntity<MoveResponseDTO> askQuestion(
+            @PathVariable String roomCode,
+            @RequestBody QuestionRequestDTO request, 
+            @AuthenticationPrincipal User player) {
+        MoveResponseDTO move = jogadaService.processQuestion(roomCode, player.getId(), request.question());
+
+        messagingTemplate.convertAndSend("/topic/game-sessions/" + roomCode + "/moves", move);
+        return ResponseEntity.ok(move);
+    }
+    
+    @PutMapping("{roomCode}/moves/{moveId}/answer-question")
+    public ResponseEntity<MoveResponseDTO> answerQuestion(
+            @PathVariable String roomCode,
+            @PathVariable Long moveId,
+            @RequestBody AnswerRequestDTO request,
+            @AuthenticationPrincipal User master) {
+        MoveResponseDTO move = jogadaService.processAnswer(moveId, master.getId(), request.answer());
+        
+        messagingTemplate.convertAndSend("/topic/game-sessions/" + roomCode + "/moves", move);
+        return ResponseEntity.ok(move);
+    }
+
 }
